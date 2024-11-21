e5ae588e1f5a2163d4db88a8af0b5a2746bd45f9
melancholiai <57397441+melancholiai@users.noreply.github.com>
2024-11-21 17:46:27 +0200

    fix(frontend): missing dynamic config for some values (#4)

--- packages/frontend/src/config/index.ts
+++ packages/frontend/src/config/index.ts
@@ -5,56 +5,89 @@ import { ConfigStore, IConfig } from './configStore';
 
 const viteConfig = import.meta.env;
 
+const PLACEHOLDER_SUFFIX = '_PLACEHOLDER';
+
+type ParseType = 'string' | 'number' | 'boolean';
+
+const isPlaceholder = (value: string | undefined): boolean => {
+  return typeof value === 'string' && value.endsWith(PLACEHOLDER_SUFFIX);
+};
+
+const parseValue = <T extends ParseType, R = T extends 'string' ? string : T extends 'number' ? number : T extends 'boolean' ? boolean : undefined>(
+  type: T,
+  value?: string
+): R | undefined => {
+  if (value === undefined || isPlaceholder(value)) {
+    return undefined;
+  }
+
+  switch (type) {
+    case 'string': {
+      return value as R;
+    }
+    case 'number': {
+      return parseInt(value) as R;
+    }
+    case 'boolean': {
+      return parseBoolean(value) as R;
+    }
+    default: {
+      return undefined;
+    }
+  }
+};
+
 export const config: IConfig = (function (): IConfig {
   const config = new ConfigStore();
 
   config.set('telemetry.logger', {
-    level: viteConfig.CONFIG_LOG_LEVEL,
-    prettyPrint: parseBoolean(viteConfig.CONFIG_LOG_PRETTY_PRINT_ENABLED),
+    level: parseValue('string', viteConfig.CONFIG_LOG_LEVEL),
+    prettyPrint: parseValue('boolean', viteConfig.CONFIG_LOG_PRETTY_PRINT_ENABLED),
   });
 
-  const retryStrategy = parseBoolean(viteConfig.CONFIG_DETILER_CLIENT_ENABLE_RETRY_STRATEGY)
-    ? {
-        delay: parseInt(viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_DELAY!),
-        isExponential: parseBoolean(viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_IS_EXPONENTIAL!),
-        retries: parseInt(viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_RETRIES!),
-        shouldResetTimeout: parseBoolean(viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_SHOULD_RESET_TIMEOUT!),
-      }
-    : undefined;
+  const retryStrategy =
+    parseValue('boolean', viteConfig.CONFIG_DETILER_CLIENT_ENABLE_RETRY_STRATEGY) ?? false
+      ? {
+          delay: parseValue('number', viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_DELAY),
+          isExponential: parseValue('boolean', viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_IS_EXPONENTIAL),
+          retries: parseValue('number', viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_RETRIES),
+          shouldResetTimeout: parseValue('boolean', viteConfig.CONFIG_DETILER_CLIENT_RETRY_STRATEGY_SHOULD_RESET_TIMEOUT),
+        }
+      : undefined;
 
   const detilerConfig: DetilerClientConfig = {
     url: viteConfig.CONFIG_DETILER_CLIENT_URL,
-    timeout: viteConfig.CONFIG_DETILER_CLIENT_TIMEOUT !== undefined ? parseInt(viteConfig.CONFIG_DETILER_CLIENT_TIMEOUT) : undefined,
-    enableRetryStrategy: parseBoolean(viteConfig.CONFIG_DETILER_CLIENT_ENABLE_RETRY_STRATEGY),
+    timeout: parseValue('number', viteConfig.CONFIG_DETILER_CLIENT_TIMEOUT),
+    enableRetryStrategy: parseValue('boolean', viteConfig.CONFIG_DETILER_CLIENT_ENABLE_RETRY_STRATEGY),
     retryStrategy,
   };
 
   const basemap = parseBoolean(viteConfig.CONFIG_APP_BASEMAP_ENABLED)
     ? {
-        url: viteConfig.CONFIG_APP_BASEMAP_URL,
-        xApiKey: viteConfig.CONFIG_APP_BASEMAP_XAPIKEY,
-        tileSize: parseInt(viteConfig.CONFIG_APP_BASEMAP_TILE_SIZE!),
-        zoomOffset: parseInt(viteConfig.CONFIG_APP_BASEMAP_ZOOM_OFFSET!),
-        desaturate: parseInt(viteConfig.CONFIG_APP_BASEMAP_DESATURATE!),
+        url: parseValue('string', viteConfig.CONFIG_APP_BASEMAP_URL),
+        xApiKey: parseValue('string', viteConfig.CONFIG_APP_BASEMAP_XAPIKEY),
+        tileSize: parseValue('number', viteConfig.CONFIG_APP_BASEMAP_TILE_SIZE),
+        zoomOffset: parseValue('number', viteConfig.CONFIG_APP_BASEMAP_ZOOM_OFFSET),
+        desaturate: parseValue('number', viteConfig.CONFIG_APP_BASEMAP_DESATURATE),
       }
     : {};
 
   const appConfig: AppConfig = {
     basemap: {
-      enabled: parseBoolean(viteConfig.CONFIG_APP_BASEMAP_ENABLED),
+      enabled: parseValue('boolean', viteConfig.CONFIG_APP_BASEMAP_ENABLED) ?? false,
       ...basemap,
     },
     style: {
-      dataAlphaChannel: viteConfig.CONFIG_APP_DATA_ALPHA_CHANNEL !== undefined ? parseInt(viteConfig.CONFIG_APP_DATA_ALPHA_CHANNEL) : undefined,
-      tilesPerPage: viteConfig.CONFIG_APP_UI_TILES_PER_PAGE !== undefined ? parseInt(viteConfig.CONFIG_APP_UI_TILES_PER_PAGE) : undefined,
+      dataAlphaChannel: parseValue('number', viteConfig.CONFIG_APP_DATA_ALPHA_CHANNEL),
+      tilesPerPage: parseValue('number', viteConfig.CONFIG_APP_UI_TILES_PER_PAGE),
     },
     kits: {
-      fetchInterval: viteConfig.CONFIG_APP_KITS_FETCH_INTERVAL !== undefined ? parseInt(viteConfig.CONFIG_APP_KITS_FETCH_INTERVAL) : undefined,
+      fetchInterval: parseValue('number', viteConfig.CONFIG_APP_KITS_FETCH_INTERVAL),
     },
     tiles: {
-      batchSize: viteConfig.CONFIG_APP_TILES_BATCH_SIZE !== undefined ? parseInt(viteConfig.CONFIG_APP_TILES_BATCH_SIZE) : undefined,
-      fetchInterval: viteConfig.CONFIG_APP_TILES_FETCH_INTERVAL !== undefined ? parseInt(viteConfig.CONFIG_APP_TILES_FETCH_INTERVAL) : undefined,
-      fetchTimeout: viteConfig.CONFIG_APP_TILES_FETCH_TIMEOUT !== undefined ? parseInt(viteConfig.CONFIG_APP_TILES_FETCH_TIMEOUT) : undefined,
+      batchSize: parseValue('number', viteConfig.CONFIG_APP_TILES_BATCH_SIZE),
+      fetchInterval: parseValue('number', viteConfig.CONFIG_APP_TILES_FETCH_INTERVAL),
+      fetchTimeout: parseValue('number', viteConfig.CONFIG_APP_TILES_FETCH_TIMEOUT),
     },
   };
 
