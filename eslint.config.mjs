747161678e539099762a90f00d64bc9e6168b1c0
NatalieShaked <shakednatalie@gmail.com>
2025-08-25 15:33:46 +0300

    fix(configurations): eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,13 @@ import tsBaseConfig from '@map-colonies/eslint-config/ts-base';
 import jestConfig from '@map-colonies/eslint-config/jest';
 import { config } from '@map-colonies/eslint-config/helpers';
 
-export default config(jestConfig, tsBaseConfig);
+// export default config(jestConfig, tsBaseConfig);
+
+const base = config(jestConfig, tsBaseConfig);
+
+export default [
+  ...base,
+  {
+    ignores: ['**/dist/**', '**/build/**', 'node_modules/**', 'coverage/**'],
+  },
+];
