5ee1307118caee5f896ecb21ca9a80c63b1b9b3f
NatalieShaked <shakednatalie@gmail.com>
2025-08-31 13:44:26 +0300

    fix(configurations): eslint error when running pull request

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,9 +1,5 @@
 import tsBase from '@map-colonies/eslint-config/ts-base';
 import { config } from '@map-colonies/eslint-config/helpers';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-
-const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
 
 export default [
   { ignores: ['**/*.js', 'dist', 'helm', '**/*.d.ts', 'vite.config.ts', 'node_modules'] },
@@ -11,9 +7,7 @@ export default [
   ...config(tsBase, {
     languageOptions: {
       parserOptions: {
-        // project: './tsconfig.lint.json',
-        projeconfigRootDt: [path.join(tsconfigRootDir, 'tsconfig.lint.json')],
-        tsconfigRootDir,
+        project: './tsconfig.lint.json',
       },
     },
   }),
