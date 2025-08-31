4daf9d14dbc8cd778e0af008e0dc1bae5c2fa734
NatalieShaked <shakednatalie@gmail.com>
2025-08-31 13:27:57 +0300

    fix(configurations): eslint error when running pull request

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,4 @@ import tsBaseConfig from '@map-colonies/eslint-config/ts-base';
 import jestConfig from '@map-colonies/eslint-config/jest';
 import { config } from '@map-colonies/eslint-config/helpers';
 
-// export default config(jestConfig, tsBaseConfig);
-
-const base = config(jestConfig, tsBaseConfig);
-
-export default [
-  ...base,
-  {
-    ignores: ['**/dist/**', '**/build/**', 'node_modules/**', 'coverage/**'],
-  },
-];
+export default config(jestConfig, tsBaseConfig);
--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,5 +1,9 @@
 import tsBase from '@map-colonies/eslint-config/ts-base';
 import { config } from '@map-colonies/eslint-config/helpers';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
 
 export default [
   { ignores: ['**/*.js', 'dist', 'helm', '**/*.d.ts', 'vite.config.ts', 'node_modules'] },
@@ -7,7 +11,9 @@ export default [
   ...config(tsBase, {
     languageOptions: {
       parserOptions: {
-        project: './tsconfig.lint.json',
+        // project: './tsconfig.lint.json',
+        projeconfigRootDt: [path.join(tsconfigRootDir, 'tsconfig.lint.json')],
+        tsconfigRootDir,
       },
     },
   }),
