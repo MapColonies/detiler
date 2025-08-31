67def2918ae01e3f3384d7373718265c59dfd53e
NatalieShaked <shakednatalie@gmail.com>
2025-08-31 13:53:22 +0300

    fix(configurations): eslint error when running pull request

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,6 @@ import tsBaseConfig from '@map-colonies/eslint-config/ts-base';
 import jestConfig from '@map-colonies/eslint-config/jest';
 import { config } from '@map-colonies/eslint-config/helpers';
 
-export default config(jestConfig, tsBaseConfig);
+// export default config(jestConfig, tsBaseConfig);
+
+export default [{ ignores: ['**/dist/**', '**/build/**', 'node_modules/**', 'coverage/**', '**/*.d.ts'] }, ...config(jestConfig, tsBaseConfig)];
